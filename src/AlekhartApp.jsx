eb7657b1444cfccb324366ee1b086de2f46d0a00
Nishim <victor19.mp@gmail.com>
2026-02-26 14:48:43 -0300

    Remove unused routes from AlekhartApp

--- src/AlekhartApp.jsx
+++ src/AlekhartApp.jsx
@@ -1,11 +1,8 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { NavBar } from "./routes/Components/NavBar"; 
 import { HomeScreen } from "./routes/HomeScreen";
-import { AboutScreen } from "./routes/AboutScreen";
 import { ContactScreen } from "./routes/ContactScreen";
 import { UsuarioProvider } from "./context/UsuarioProvider";
-import { LoginScreen } from "./routes/LoginScreen"; 
-import { Gracias } from './routes/Components/Gracias';
 
 export const AlekhartApp = () => {
   return (
@@ -13,9 +10,6 @@ export const AlekhartApp = () => {
       <NavBar />
       <Routes>
         <Route path="/" element={<HomeScreen />} />
-        <Route path="/Gracias" element={<Gracias />} />
-        <Route path="/login" element={<LoginScreen />} />
-        <Route path="/about" element={<AboutScreen />} />
         <Route path="/contact" element={<ContactScreen />} /> 
         <Route path="/*" element={<Navigate to="/" />} />
       </Routes>
